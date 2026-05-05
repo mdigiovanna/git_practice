059d2254e6011019fe85b05a8e89218d4177fc44
mdigiovanna <michaeldigiovanna2@gmail.com>
2026-05-05 09:46:41 -0400

    Update hello.py locally

--- hello.py
+++ hello.py
@@ -1,5 +1,2 @@
-print("Hello from both GitHub and local computer!")
-print("This version combines both edits")
-print("Local version: edited locally")
-print("GitHub version: edited on GitHub")
-print("Added one more hello!")
+print("Hello from VS Code!")
+print("This version was edited locally")
